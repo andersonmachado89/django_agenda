61fd37e40417457f97eb1c6ae13a4cac77791130
andersonmachado89 <anderspm@br.ibm.com>
2024-04-01 22:24:53 -0300

    finalizando models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    # list_per_page = 1
+    
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
